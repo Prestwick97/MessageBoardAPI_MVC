3a166b2501565ec681d88623fd5acec895f839f1
Jose Amesquita <joseamesquita11@gmail.com>
2020-04-01 10:19:10 -0700

    add api versioning
    Co-authored-by: Patrick DElaney <prestwick97@gmail.com>
    Co-authored-by: Rachel Schieferstein <violenza@gmail.com>

--- MessageBoard/Controllers/ValuesController.cs
+++ MessageBoard/Controllers/ValuesController.cs
@@ -6,41 +6,81 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MessageBoard.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ValuesController : ControllerBase
+  [ApiVersion("1.0")]
+  [Route("api/{v:apiVersion}/values")]
+  //api/1.0/values
+  [ApiController]
+  public class ValuesController : ControllerBase
+  {
+    // GET api/values
+    [HttpGet]
+    public ActionResult<IEnumerable<string>> Get()
     {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-            Console.WriteLine(value);
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+      return new string[] { "Value1 from Version1 ", "value2 from Version 1" };
     }
+
+    // GET api/values/5
+    [HttpGet("{id}")]
+    public ActionResult<string> Get(int id)
+    {
+      return "value";
+    }
+
+    // POST api/values
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+      Console.WriteLine(value);
+    }
+
+    // PUT api/values/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/values/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+  }
+  [ApiVersion("2.0")]
+  [Route("api/{v:apiVersion}/values")]
+  [ApiController]
+  public class ValuesTwoController : ControllerBase
+  {
+    // GET api/values
+    [HttpGet]
+    public ActionResult<IEnumerable<string>> Get()
+    {
+      return new string[] { "value1 from Version 2", "value2 from Version 2" };
+    }
+
+    // GET api/values/5
+    [HttpGet("{id}")]
+    public ActionResult<string> Get(int id)
+    {
+      return "value";
+    }
+
+    // POST api/values
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+      Console.WriteLine(value);
+    }
+
+    // PUT api/values/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/values/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+  }
 }
