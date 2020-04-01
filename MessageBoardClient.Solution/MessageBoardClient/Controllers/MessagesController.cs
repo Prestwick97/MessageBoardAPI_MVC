c715a15e0edc22d64686542c2e29f4901aeae855
Jose Amesquita <joseamesquita11@gmail.com>
2020-04-01 13:20:04 -0700

    remove group duplicates
    Co-authored-by: Patrick DElaney <prestwick97@gmail.com>
    Co-authored-by: Rachel Schieferstein <violenza@gmail.com>

--- MessageBoardClient.Solution/MessageBoardClient/Controllers/MessagesController.cs
+++ MessageBoardClient.Solution/MessageBoardClient/Controllers/MessagesController.cs
@@ -18,16 +18,15 @@ namespace MessageBoardClient.Controllers
 
     public IActionResult Groups()
     {
-      var allMessages = Message.GetMessages().Distinct();
+      var allMessages = Message.GetMessages().OrderBy(message => message.Group);
       return View(allMessages);
-
     }
 
-    //     public IactionResult Group()
-    //     {
-    //       var group = Message.GetGroup();
-    //       return View(allMessages);
-    //     }
+    // public IActionResult ----()
+    // {
+    //   var group = Message.GetGroup();
+    //   return View(allMessages);
+    // }
 
     // public IActionResult Details(int id)
     // {
